efdb5732554c1489858028727f9c4208ba8ac486
Devin McElheran <devin.mcelheran@voyav.com>
2025-08-15 14:31:27 -0400

    Fixed issue with VerifyDriver parameter name misalignment.

--- InvocationContexts/VerifyInvocation.cs
+++ InvocationContexts/VerifyInvocation.cs
@@ -66,7 +66,7 @@ namespace Printune
             
             try
             {
-                _name = (string)parameterFile.GetParameter("Driver");
+                _name = (string)parameterFile.GetParameter("Name");
             }
             catch (KeyNotFoundException)
             {
